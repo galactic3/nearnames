9f605f5af20f654d716880151dedea1621d53398
Alex Aunter <lxaunter@gmail.com>
2021-11-15 23:15:28 +0300

    add profile get draft

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,15 +1,12 @@
+mod profile;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{UnorderedMap};
-use near_sdk::{
-    env, near_bindgen, setup_alloc,
-    AccountId, Balance,
-};
+use near_sdk::collections::UnorderedMap;
+use near_sdk::json_types::{ValidAccountId, WrappedBalance};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, setup_alloc, AccountId, Balance};
 
-pub struct Profile {
-    pub available_rewards: Balance,
-    pub profit_received: Balance,
-}
+use crate::profile::*;
 
 pub type LotId = AccountId;
 pub type ProfileId = AccountId;
@@ -41,40 +38,62 @@ impl Contract {
 
 #[cfg(test)]
 mod tests {
+    use std::convert::TryInto;
+
     use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::MockedBlockchain;
     use near_sdk::{testing_env, VMContext};
 
-    // mock the context for testing, notice "signer_account_id" that was accessed above from env::
-    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
-        VMContext {
-            current_account_id: "alice_near".to_string(),
-            signer_account_id: "bob_near".to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "carol_near".to_string(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
+    fn get_context_simple(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("alice_near".try_into().unwrap())
+            .is_view(is_view)
+            .build()
     }
 
     #[test]
-    fn hello() {
-        let context = get_context(vec![], false);
+    fn profile_get_missing() {
+        let context = get_context_simple(false);
         testing_env!(context);
         let contract = Contract::default();
+
+        assert!(
+            contract.profile_get("alice".try_into().unwrap()).is_none(),
+            "Expected get_profile to return None",
+        );
+    }
+
+    #[test]
+    fn profile_get_present() {
+        let context = get_context_simple(false);
+        testing_env!(context);
+        let mut contract = Contract::default();
+
+        let yocto_in_near: u128 = 10u128.pow(24);
+
+        let profile_id: AccountId = "alice".into();
+        let available_rewards: u128 = yocto_in_near * 2;
+        let profit_received: u128 = yocto_in_near * 3;
+        let profile: Profile = Profile {
+            available_rewards,
+            profit_received,
+        };
+        contract.internal_profile_save_or_panic(&profile_id, &profile);
+
+        let response: Option<ProfileView> = contract.profile_get("alice".try_into().unwrap());
+        assert!(response.is_some());
+        let response = response.unwrap();
+
         assert_eq!(
-            "hello, carol_near".to_string(),
-            contract.hello(),
+            response.available_rewards,
+            available_rewards.into(),
+            "available_rewards mismatch"
+        );
+        assert_eq!(
+            response.profit_received,
+            profit_received.into(),
+            "profit_received mismatch"
         );
     }
 }
---
+++ contract/src/profile.rs
@@ -0,0 +1,45 @@
+use crate::*;
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct Profile {
+    pub available_rewards: Balance,
+    pub profit_received: Balance,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct ProfileView {
+    pub available_rewards: WrappedBalance,
+    pub profit_received: WrappedBalance,
+}
+
+impl From<&Profile> for ProfileView {
+    fn from(p: &Profile) -> Self {
+        Self {
+            available_rewards: p.available_rewards.into(),
+            profit_received: p.profit_received.into(),
+        }
+    }
+}
+
+impl Contract {
+    pub(crate) fn internal_profile_extract_or_create(&mut self, profile_id: &ProfileId) -> Profile {
+        self.profiles
+            .remove(&profile_id)
+            .unwrap_or_else(|| Profile {
+                available_rewards: 0,
+                profit_received: 0,
+            })
+    }
+
+    pub(crate) fn internal_profile_save_or_panic(&mut self, profile_id: &ProfileId, profile: &Profile) {
+        assert!(self.profiles.insert(profile_id, profile).is_none());
+    }
+}
+
+#[near_bindgen]
+impl Contract {
+    pub fn profile_get(&self, profile_id: ValidAccountId) -> Option<ProfileView> {
+        self.profiles.get(profile_id.as_ref()).map(|p| (&p).into())
+    }
+}
