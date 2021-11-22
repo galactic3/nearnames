0f18afec991ede0da236302179042abb58e69059
Alex Aunter <lxaunter@gmail.com>
2021-11-22 23:34:08 +0300

    add init helper

--- contract/tests/general.rs
+++ contract/tests/general.rs
@@ -1,6 +1,22 @@
-use near_sdk_sim::init_simulator;
+use near_sdk_sim::{call, view, to_yocto, deploy, init_simulator, ContractAccount, UserAccount};
+use near_sdk::{Balance, AccountId};
 
-#[test]
-fn simulate_lot_list() {
+use marketplace::{ContractContract, LotView, WrappedBalance};
+
+near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
+    COUNTER_BYTES => "res/marketplace.wasm",
+}
+
+fn init() -> (UserAccount, ContractAccount<ContractContract>) {
     let root = init_simulator(None);
+
+    // Deploy the compiled Wasm bytes
+    let counter: ContractAccount<ContractContract> = deploy!(
+        contract: ContractContract,
+        contract_id: "marketplace".to_string(),
+        bytes: &COUNTER_BYTES,
+        signer_account: root
+    );
+
+    (root, counter)
 }
