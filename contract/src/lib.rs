3e5300fb76d1b5b3ea660d1412af395448990bf8
Alex Aunter <lxaunter@gmail.com>
2021-12-29 21:22:42 +0300

    extract common test helpers to separate file

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -113,48 +113,11 @@ impl Contract {
 }
 
 #[cfg(test)]
-mod tests {
-    pub use super::*;
+mod tests;
 
-    pub use near_sdk::test_utils::VMContextBuilder;
-    pub use near_sdk::{testing_env, VMContext};
-    pub use near_sdk_sim::{to_nanos, to_ts, to_yocto};
-
-    pub fn get_context_view(time_now: Timestamp) -> VMContext {
-        VMContextBuilder::new()
-            .is_view(true)
-            .block_timestamp(time_now)
-            .build()
-    }
-
-    pub fn get_context_call(time_now: Timestamp, caller_id: &LotId) -> VMContext {
-        VMContextBuilder::new()
-            .predecessor_account_id(caller_id.clone())
-            .is_view(false)
-            .block_timestamp(time_now)
-            .build()
-    }
-
-    pub fn get_context_pay(
-        time_now: Timestamp,
-        caller_id: &ProfileId,
-        attached_deposit: Balance,
-    ) -> VMContext {
-        VMContextBuilder::new()
-            .predecessor_account_id(caller_id.clone())
-            .is_view(false)
-            .attached_deposit(attached_deposit)
-            .block_timestamp(time_now)
-            .build()
-    }
-
-    pub fn build_contract() -> Contract {
-        Contract::new(
-            FractionView { num: 1, denom: 10 },
-            FractionView { num: 1, denom: 5 },
-            FractionView { num: 4, denom: 5 },
-        )
-    }
+#[cfg(test)]
+mod contract_tests {
+    use crate::tests::*;
 
     #[test]
     fn contract_config_get() {
