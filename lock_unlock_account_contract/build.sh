650bb23a2ed162ebaece00eb9642999edeee683c
Alex Aunter <lxaunter@gmail.com>
2022-01-07 13:18:09 +0300

    add docker build files

--- lock_unlock_account_contract/build.sh
+++ lock_unlock_account_contract/build.sh
@@ -3,3 +3,4 @@ set -e
 cd "`dirname $0`"
 cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/lock_unlock_account.wasm ./res/
+./minify.sh res/lock_unlock_account.wasm res/lock_unlock_account.min.wasm
