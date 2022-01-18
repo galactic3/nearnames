16766c3ab630d3fd17fb11cbc2a70b4f997f7497
Alex Aunter <lxaunter@gmail.com>
2022-01-18 19:29:47 +0300

    fix build sh res timestamp setting

--- contract/build.sh
+++ contract/build.sh
@@ -5,4 +5,5 @@ package_name=$(cat Cargo.toml | sed -ne 's/^name = "\(.*\)"$/\1/p')
 rm -f "./res/$package_name.wasm"
 
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
-rsync -t "target/wasm32-unknown-unknown/release/$package_name.wasm" ./res/
+cp "target/wasm32-unknown-unknown/release/$package_name.wasm" "./res/$package_name.wasm"
+touch -r target/wasm32-unknown-unknown/release/$package_name.wasm ./res/$package_name.wasm
